13daba850510025b9913ee48bc043ddfc3a46da2
mohamedvol1 <70990780+mohamedvol1@users.noreply.github.com>
2022-05-23 05:57:04 +0200

    added databese.sql file

---
+++ src/database/database.sql
@@ -0,0 +1,13 @@
+create database Blogs_db;
+
+CREATE TABLE public.blogs (
+  author_id character varying(255) NOT NULL,
+  author character varying(255) NOT NULL,
+  blog_content character varying(8000) NULL,
+  created_at timestamp without time zone NOT NULL,
+  blog_id integer NOT NULL
+);
+ALTER TABLE
+  public.blogs
+ADD
+  CONSTRAINT blogs_pkey PRIMARY KEY (blog_id)
